7090cf665cace705be80373f89494831ab5ea132
Thomas Mumena <mumenathomas@gmail.com>
2024-03-03 18:08:31 +0200

    create and style the footer. Style the scrollbar and scrollbar-thumb. For scrollY >= 350 of the viewport, make a styled link appear just above the bottom of the viewport on the right, to scroll to the top of the page.

--- js/main.js
+++ js/main.js
@@ -32,3 +32,12 @@ const blurHeader = () =>{
     : header.classList.remove('blur-header')
   }
   this.addEventListener('scroll', blurHeader)
+
+  const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+    this.scrollY >= 350 
+    ? scrollUp.classList.add('show-scroll-up')
+    : scrollUp.classList.remove('show-scroll-up')
+  }
+  this.addEventListener('scroll', scrollUp)
